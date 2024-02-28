699ef8caca0f51a8c66559156ea431a271b15365
Aira Catig <rcatig@uci.edu>
2024-02-27 16:21:33 -0800

    Added docstring to DSPProtocol class

--- Desktop/Assignment3/ds_protocol.py
+++ Desktop/Assignment3/ds_protocol.py
@@ -6,42 +6,66 @@
 
 import json
 from collections import namedtuple
-import Profile
 import time
 
 
-DataTuple = namedtuple("DataTuple", ["join", "post", "bio", "error", "ok"])
-
-
 class DSPProtocol:
+    """
+    Format protocol to send to online server as 
+    a dictionary.
+    """
     def __init__(self, user: str = None, password:
                  str = None, bio: str = None):
         self.user = user
         self.password = password
 
     def join(self, user: str, password: str):
+        """
+        Create message as a dictionary with a 
+        username, password, and token generated
+        from the server to join the server.
+        """
         result = {"join": {"username": user,
                   "password": password, "token": ":"}}
         return json.dumps(result)
 
     def post(self, token: str, entry: str):
+        """
+        Create message as a dictionary with the
+        post entry, token from the server, and timestamp
+        to post the entry to the server.
+        """
         timestamp = time.time()
         result = {"token": token, "post": {"entry":
                   entry, "timestamp": timestamp}}
         return json.dumps(result)
 
     def bio(self, token: str, entry: str):
+        """
+        Create message as a dictionary with the bio
+        entry, token from the server, and timestamp 
+        to add bio the server.
+        """
         timestamp = time.time()
         result = {"token": token, "bio": {"entry": entry,
                   "timestamp": timestamp}}
         return json.dumps(result)
 
     def error(self, error, message):
+        """
+        Create message as a dictionary that will return
+        the type of error along with the error message.
+        """
         result = {"response": {"type": error, "message": message}}
         return json.dumps(result)
 
     def ok(self, error, message, token):
-        result = {"response": {"type": error, "message":
+        """
+        Create message as a dictionary after each message
+        sent to the server is allowed. Will include the type,
+        message, and token recieved from server.
+        """
+        result = {"response": {"type": "ok", "message":
                   message, "token": token}}
         return json.dumps(result)
 
